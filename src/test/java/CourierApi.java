5ed3bccb9e7949f4eb80ca8ca4e5f92388e2a22f
Elena Prokopeva <g777elenagromova@yandex.ru>
2023-01-08 13:53:04 +0600

    2023-01-08 финальное задание спринт 7: ИСПРАВЛЕНИЕ ЗАМЕЧАНИЙ

---
+++ src/test/java/CourierApi.java
@@ -0,0 +1,50 @@
+import io.qameta.allure.Step;
+import org.example.Courier;
+import static org.hamcrest.Matchers.*;
+import static org.apache.http.HttpStatus.*;
+
+public class CourierApi extends BaseApi{
+
+    private Courier courier;
+
+    @Step("Создать курьера и проверить статус ответа")
+    public Courier createCourier(){
+        this.courier = new Courier("ya", "1234", "saske");
+        requestSpecification
+                .given()
+                .body(courier) // заполни body
+                .when()
+                .post(COURIER_CREATE_ENDPOINT) // отправь запрос на ручку
+                .then().assertThat().body("ok", is(true))
+                .and()
+                .statusCode(SC_CREATED);
+        return courier;
+    }
+
+    @Step("Авторизоваться под курьером и получить его id")
+    public void loginCourier(){
+        requestSpecification
+                .given()
+                .body(courier) // заполни body
+                .when()
+                .post(COURIER_LOGIN_ENDPOINT) // отправь запрос на ручку
+                .then().assertThat().body("id", notNullValue())
+                .and()
+                .statusCode(SC_OK);
+    }
+
+    @Step("Удалить курьера по id")
+    public void deleteCourier(){
+        if (courier != null) {
+            Integer courierId =
+                    requestSpecification.given()
+                            .body(courier) // заполни body
+                            .when()
+                            .post(COURIER_LOGIN_ENDPOINT) // отправь запрос на ручку
+                            .then().extract().body().path("id");
+            requestSpecification.given()
+                    .delete(COURIER_CREATE_ENDPOINT + "/{id}", courierId.toString()) // отправка DELETE-запроса
+                    .then().assertThat().statusCode(SC_OK); // проверка, что сервер вернул код 200
+        }
+    }
+}
